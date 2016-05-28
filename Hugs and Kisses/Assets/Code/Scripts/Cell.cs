2abaa3caa0416434495f731ea2aac2b79f9f18e0
Achromic <music1384@yahoo.co.uk>
2016-05-28 12:13:41 +0100

    Designed UI system and created prefabs from it. Basic gameplay flow has now been implemented.

---
+++ Hugs and Kisses/Assets/Code/Scripts/Cell.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Cell : MonoBehaviour
+{
+	[HideInInspector] public int PlayerOwner = -1;
+
+	private Sprite m_HugSprite = null;
+	private Sprite m_KissSprite = null;
+	private Button m_Button = null;
+	private Image m_Image = null;
+	private GameHandler m_GameHandler = null;
+
+	public void Initialise (GameHandler handler)
+	{
+		AssignComponentReferences (handler);
+		SetCellDefaults ();
+	}
+
+	void AssignComponentReferences (GameHandler handler)
+	{
+		m_Button = GetComponent<Button> ();
+		m_Image = GetComponent<Image> ();
+		m_GameHandler = handler;
+		m_HugSprite = m_GameHandler.HugSprite;
+		m_KissSprite = m_GameHandler.KissSprite;
+	}
+
+	void SetCellDefaults ()
+	{
+		PlayerOwner = -1;
+		m_Button.interactable = true;
+	}
+
+	public void CellClick ()
+	{
+		PlayerOwner = m_GameHandler.GetCurrentPlayer ();
+		m_GameHandler.EndTurn ();
+		UpdateCell ();
+	}
+
+	void UpdateCell ()
+	{
+		m_Image.sprite = CurrentPlayerSprite ();
+		m_Button.interactable = false;
+	}
+
+	Sprite CurrentPlayerSprite ()
+	{
+		if(PlayerOwner == 0)
+		{
+			return m_HugSprite;
+		}
+		else
+		{
+			return m_KissSprite;
+		}
+	}
+}
---
+++ Hugs and Kisses/Assets/Code/Scripts/GameHandler.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameHandler : MonoBehaviour
+{
+	public Sprite HugSprite = null;
+	public Sprite KissSprite = null;
+	[SerializeField] private GameObject m_BoardCellsPrefab = null;
+
+	private int m_PlayerTurn = 1;
+	private Cell[] m_Cells = null;
+
+	void Awake ()
+	{
+		AssignComponentReferences ();
+		InitialiseGame ();
+	}
+
+	void AssignComponentReferences ()
+	{
+		m_Cells = new Cell[m_BoardCellsPrefab.transform.childCount];
+
+		for (int i = 0; i < m_Cells.Length; i++)
+		{
+			m_Cells [i] = m_BoardCellsPrefab.transform.GetChild (i).GetComponent<Cell> ();
+		}
+	}
+
+	void InitialiseGame ()
+	{
+		foreach (Cell cell in m_Cells)
+		{
+			cell.Initialise (this);
+		}
+	}
+
+	public int GetCurrentPlayer ()
+	{
+		return m_PlayerTurn;
+	}
+
+	public void EndTurn ()
+	{
+		CheckForWinCondition ();
+		SwitchTurn ();
+	}
+
+	void SwitchTurn ()
+	{
+		m_PlayerTurn = (m_PlayerTurn == 1) ? 0 : 1;
+	}
+
+	void CheckForWinCondition ()
+	{
+		VerticalConditions ();
+		HorizontalConditions ();
+		DiagonalConditions ();
+	}
+
+	void VerticalConditions ()
+	{
+		if(m_Cells[0].PlayerOwner == m_PlayerTurn && m_Cells[1].PlayerOwner == m_PlayerTurn && m_Cells[2].PlayerOwner == m_PlayerTurn)
+		{
+			GameOver ();
+		}
+		else if(m_Cells[3].PlayerOwner == m_PlayerTurn && m_Cells[4].PlayerOwner == m_PlayerTurn && m_Cells[5].PlayerOwner == m_PlayerTurn)
+		{
+			GameOver ();
+		}
+		else if(m_Cells[6].PlayerOwner == m_PlayerTurn && m_Cells[7].PlayerOwner == m_PlayerTurn && m_Cells[8].PlayerOwner == m_PlayerTurn)
+		{
+			GameOver ();
+		}
+	}
+
+	void HorizontalConditions ()
+	{
+		if(m_Cells[0].PlayerOwner == m_PlayerTurn && m_Cells[3].PlayerOwner == m_PlayerTurn && m_Cells[6].PlayerOwner == m_PlayerTurn)
+		{
+			GameOver ();
+		}
+		else if(m_Cells[1].PlayerOwner == m_PlayerTurn && m_Cells[4].PlayerOwner == m_PlayerTurn && m_Cells[7].PlayerOwner == m_PlayerTurn)
+		{
+			GameOver ();
+		}
+		else if(m_Cells[2].PlayerOwner == m_PlayerTurn && m_Cells[5].PlayerOwner == m_PlayerTurn && m_Cells[8].PlayerOwner == m_PlayerTurn)
+		{
+			GameOver ();
+		}
+	}
+
+	void DiagonalConditions ()
+	{
+		if(m_Cells[0].PlayerOwner == m_PlayerTurn && m_Cells[4].PlayerOwner == m_PlayerTurn && m_Cells[8].PlayerOwner == m_PlayerTurn)
+		{
+			GameOver ();
+		}
+		else if(m_Cells[6].PlayerOwner == m_PlayerTurn && m_Cells[4].PlayerOwner == m_PlayerTurn && m_Cells[2].PlayerOwner == m_PlayerTurn)
+		{
+			GameOver ();
+		}
+	}
+
+	void GameOver ()
+	{
+		foreach (Cell cell in m_Cells)
+		{
+			cell.gameObject.GetComponent<Button> ().interactable = false;
+		}
+	}
+}
